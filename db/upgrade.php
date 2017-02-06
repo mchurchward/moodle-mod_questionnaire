d47cde30da14889b9e13fab00d5b730b93dc6b52
kyro46 <cjobst@wifa.uni-leipzig.de>
2017-02-06 14:21:51 +0100

    Add option for advanced navigation to existing dropdown, add table in DB
    The new table holds information about dependend questions and the
    corresponding answer. A question can have multiple dependencies. In
    dependlogic a value can specify wether the dependchoice is handled as
    false or true condition (go to this question if the answer IS NOT this
    choice-id / if the anser IS this choice-id).

--- db/upgrade.php
+++ db/upgrade.php
@@ -550,7 +550,35 @@ function xmldb_questionnaire_upgrade($oldversion=0) {
         // Questionnaire savepoint reached.
          upgrade_mod_savepoint(true, 2016020204, 'questionnaire');
     }
-
+    
+    // Adding table for advanced dependencies
+    if ($oldversion < 2016111103) {
+    
+    	// Define table questionnaire_dependencies to be created.
+    	$table = new xmldb_table('questionnaire_dependencies');
+    	
+    	// Adding fields to table questionnaire_depenencies.
+    	$table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+    	$table->add_field('adv_dependquestion', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+    	$table->add_field('adv_dependchoice', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+    	$table->add_field('adv_dependlogic', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0');
+    	$table->add_field('question_id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+    	
+    	// Adding keys to table questionnaire_depenencies.
+    	$table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+    	
+    	// Adding indexes to table questionnaire_depenencies.
+    	$table->add_index('quest_dependencie_quesidx', XMLDB_INDEX_NOTUNIQUE, array('question_id'));
+    	
+    	// Conditionally launch create table for questionnaire_depenencies.
+    	if (!$dbman->table_exists($table)) {
+    		$dbman->create_table($table);
+    	}
+    	
+    	// Questionnaire savepoint reached.
+    	upgrade_mod_savepoint(true, 2016111103, 'questionnaire');
+    }
+    
     return $result;
 }
 
