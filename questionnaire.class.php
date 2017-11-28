3e23b5d48853304b69353ac3e1e337ff6f0982dd
Mike Churchward <mike.churchward@poetgroup.org>
2017-11-28 15:58:37 -0500

    Poet - Cleaning up some syntax issues.

--- questionnaire.class.php
+++ questionnaire.class.php
@@ -3208,7 +3208,7 @@ class questionnaire {
 
         for ($section = 1; $section <= $this->survey->feedbacksections; $section++) {
             // get feedback messages only for this sections
-            if(($filteredsections != null) && !in_array($section, $filteredsections)){
+            if (($filteredsections != null) && !in_array($section, $filteredsections)) {
                 continue;
             }
             foreach ($fbsections as $key => $fbsection) {
